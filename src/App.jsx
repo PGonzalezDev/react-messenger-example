f63b2667fc48ed7845fcf7d3809dcf502fbb51c9
PGonzalezDev <pgonzalezdeveloper@gmail.com>
2024-08-14 17:14:45 -0300

    add login minimal behavior

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import './App.css'
 import Login from './login/Login.jsx'
 
 function App() {
+  const logoutHandler = () =>{
+    localStorage.clear()
+    window.location.reload()
+  }
+  const user = localStorage.getItem('username')
+
   return (
     <>
-      <Login />
+      { !user && <Login />}
+      { user && 
+          <>
+            <div>Bienvenido a React Messenger</div><br />
+            <button onClick={logoutHandler}>Cerrar Sesion</button>
+          </>
+      }
     </>
   )
 }
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
+
 const Login = function() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        localStorage.setItem('username', username);
+        window.location.replace("/Home");
+    }
+
     return (
         <>
-        <h3 className="h3 mb-3 fw-normal">Iniciar Sesión</h3>
-        <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label for="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label for="floatingPassword">Password</label>
-        </div>
+            <h3 className="h3 mb-3 fw-normal">Iniciar Sesión</h3>
+            <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="floatingInput" onChange={(e) => setUsername(e.target.value)} placeholder="name@example.com" />
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div className="form-floating">
+                <button onClick={handleLogin}>Login</button>
+            </div>
         </>
     )
 }
